579ba4bccaf2246b1da04800f9e310f20dcc23c9
eldar1111111 <eldar1111111@mail.ru>
2016-06-06 20:12:47 +0300

    Created project

---
+++ src/main/java/variablleswork/Tips.java
@@ -0,0 +1,18 @@
+package variablleswork;
+
+/**
+ * Created by ХХХ on 06.06.2016.
+ */
+public class Tips {
+    public static void main(String[] args) {
+        int a = 1;
+        short b = 50;
+        long c = 66666666666666L;
+        byte d = 100;
+        double f = 105.456;
+        boolean g = true;
+        char h = ('x');
+        String v = ("Джава");
+        System.out.println(a + " " + b + " " + c + " " + d + " " + f + " " + " " + g + " " + h + " " + " " + v);
+    }
+}
